2a8575ad44524f1646000f6d4a830f3ce59c826a
Jens Karsø <jens.karsoe@gmail.com>
2023-10-09 21:05:01 +0200

    specials and check

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, booleanAttribute } from '@angular/core';
 import { Guid } from "guid-typescript";
 import {Clipboard} from '@angular/cdk/clipboard';
 @Component({
@@ -16,15 +16,38 @@ export class AppComponent {
   numbers : string = '0123456789';
   constants : string = 'bcdfghjklmnopqrstvwxz';
   vovels : string = 'aeiouy';
-
+  specials : string = '!#&?£$€+-*';
 
   generatePassword(length : string) {
     this.createPassword(+length).then( o => this.password=o);
   }
 
-   createPassword = async (length: number): Promise<string> => {
+  check() : boolean {
+    let result : number = 0;
+    if (this.has(this.numbers))
+      result++;
+    if (this.has(this.constants+this.vovels))
+      result++;
+    if (this.has(this.constants.toLocaleUpperCase()+this.vovels.toLocaleUpperCase()))
+      result++;
+    if (this.has(this.specials))
+      result++;
+    return result == 4;
+  }
 
-    const characters = this.numbers+this.constants+this.vovels+this.constants.toLocaleUpperCase()+this.vovels.toLocaleUpperCase();
+  has(testStr : string) : boolean {
+    for(var i=0; i<this.password.length-1; i++)
+    {
+      if (testStr.indexOf(this.password[i])>-1) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  createPassword = async (length: number): Promise<string> => {
+
+    const characters = this.numbers+this.constants+this.vovels+this.constants.toLocaleUpperCase()+this.vovels.toLocaleUpperCase()+this.specials;
     const crypto = window.crypto || (window as any).msCrypto; // For compatibility with IE11.
     const buffer = new Uint8Array(length);
     // Generate random values and store them in the buffer.
